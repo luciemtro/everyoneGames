42a2180cc2be577a4be88ddb317156e3fbbdad3c
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-07-18 12:00:00 +0200

    bouton home

--- src/NavTop.jsx
+++ src/NavTop.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const NavTop = () => {
     return(
  
         <nav> 
-            <i class="fa-solid fa-house"></i>
+            <Link to="/"><i class="fa-solid fa-house"></i></Link>         
         </nav>
 
     )
